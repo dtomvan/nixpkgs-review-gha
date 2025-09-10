e9d8f717a88d23bfca178b10b2fe7d252e72e9e0
Tom van Dijk <18gatenmaker6@gmail.com>
2025-09-10 12:36:00 +0200

    ci: add actionlint and shellcheck

--- treefmt.nix
+++ treefmt.nix
@@ -11,6 +11,19 @@
     "LICENSE"
   ];
 
+  formatter.actionlint = {
+    command = lib.getExe pkgs.actionlint;
+    includes = [
+      ".github/workflows/*.yml"
+      ".github/workflows/*.yaml"
+    ];
+    options = [
+      "-verbose"
+      "-shellcheck"
+      (lib.getExe pkgs.shellcheck)
+    ];
+  };
+
   formatter.nixfmt = {
     command = lib.getExe pkgs.nixfmt;
     includes = [ "*.nix" ];
